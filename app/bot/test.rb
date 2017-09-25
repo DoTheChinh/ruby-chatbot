6c98b1d48319d17d36e8f29f6b965de44043fd8f
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-09-25 14:20:52 +0200

    Refactor

--- app/bot/test.rb
+++ app/bot/test.rb
@@ -4,7 +4,14 @@ Bot.on :message do |message|
   message.mark_seen
   if message.text.include? 'gif'
     message.typing_on
-    message.reply(attachment: { type: 'image', payload: { url: GifFinder.gif_url } })
+    message.reply(
+      attachment: {
+        type: 'image',
+        payload: {
+          url: GifFinder.gif_url
+        }
+      }
+    )
   else
     message.reply(text: 'Hello!')
   end
