c4d5db38b500385f96be30c11cb7123aff9eb1af
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-09-26 11:14:28 +0200

    Refactor

--- app/services/stack_finder.rb
+++ app/services/stack_finder.rb
@@ -1,51 +1,46 @@
 class StackFinder
-  SEARCH_URL_PREFIX = 'https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle='
-  SEARCH_URL_SUFIX = '&site=stackoverflow'
-  WORDS_TO_REMOVE = %w{how to can does}
+  SEARCH_URL_PREFIX = 'https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle='.freeze
+  SEARCH_URL_SUFIX = '&site=stackoverflow'.freeze
+  WORDS_TO_REMOVE = %w{how to can does}.freeze
 
-  ANSWER_UR_PREFIX = 'https://api.stackexchange.com/2.2/answers/'
-  ANSWER_URL_SUFIX = '?order=desc&sort=activity&site=stackoverflow&filter=!-*f(6sFKmjY2'
-
-  attr_reader :answer
+  ANSWER_UR_PREFIX = 'https://api.stackexchange.com/2.2/answers/'.freeze
+  ANSWER_URL_SUFIX = '?order=desc&sort=activity&site=stackoverflow&filter=!-*f(6sFKmjY2'.freeze
 
   def initialize question
-    split_question = question.chomp('?').split(' ')
-    filtered_query = split_question - WORDS_TO_REMOVE
-    @query = filtered_query.join('%20')
+    @question = question
+    @query = prepare_query
   end
 
   def find_answer
-    search_results.any? do |result|
-      !closed?(result) && @answer_id = accepted_answer(result)
+    search_results.take_while do |result|
+      if result.dig('accepted_answer_id') && !result.dig('closed_date')
+        @answer_id = result.dig('accepted_answer_id')
+      end
     end
-    @answer = parse_answer if @answer_id.present?
+    parse_answer
+  end
+
+  def answer
+    @answer ||= find_answer
   end
 
   private
 
-  def search_url
-    SEARCH_URL_PREFIX + @query + SEARCH_URL_SUFIX
-  end
+  attr_reader :question
 
-  def answer_url
-    ANSWER_UR_PREFIX + @answer_id.to_s + ANSWER_URL_SUFIX
+  def prepare_query
+    split_question = question.chomp('?').split(' ')
+    filtered_query = split_question - WORDS_TO_REMOVE
+    filtered_query.join('%20')
   end
 
   def search_results
-    @search_results ||= parsed_get_request(search_url).parse.dig('items')
-  end
-
-  def closed? result_item
-    result_item.dig('closed_date')
-  end
-
-  def accepted_answer result_item
-    result_item.dig('accepted_answer_id')
+    @search_results ||= parsed_get_request(search_url).dig('items')
   end
 
   #TODO: Parse markdown to plain text, gem: https://github.com/vmg/redcarpet
   def parse_answer
-    answer = parsed_get_request(answer_url).parse.dig('items').first
+    answer = parsed_get_request(answer_url).dig('items').first
     {
       title: answer.dig('title'),
       body: answer.dig('body_markdown')
@@ -53,6 +48,14 @@ class StackFinder
   end
 
   def parsed_get_request url
-    HTTP.use(:auto_inflate).headers('Accept-Encoding' => 'gzip').get(url)
+    HTTP.use(:auto_inflate).headers('Accept-Encoding' => 'gzip').get(url).parse
+  end
+
+  def search_url
+    SEARCH_URL_PREFIX + @query + SEARCH_URL_SUFIX
+  end
+
+  def answer_url
+    ANSWER_UR_PREFIX + @answer_id.to_s + ANSWER_URL_SUFIX
   end
 end
