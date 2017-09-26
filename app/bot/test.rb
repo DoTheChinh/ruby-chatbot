74b456d3babb5b19cbe38bf2489c745da2872308
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-09-26 10:38:08 +0200

    Remove gifs from test bot

--- app/bot/test.rb
+++ app/bot/test.rb
@@ -2,17 +2,16 @@ include Facebook::Messenger
 
 Bot.on :message do |message|
   message.mark_seen
-  if message.text.include? 'gif'
+  if message.text.end_with? '?'
     message.typing_on
+
+    stack = StackFinder.new message.text
+    stack.find_answer
+
     message.reply(
-      attachment: {
-        type: 'image',
-        payload: {
-          url: GifFinder.new.gif_by_tag
-        }
-      }
+      text: stack.answer.present? ? stack.answer[:body] : "Couldn't find any answers for You, sorry :c"
     )
   else
-    message.reply(text: 'Hello!')
+    message.reply(text: 'Hi! Ask me some questions please')
   end
 end
