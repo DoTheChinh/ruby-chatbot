20d6313b12f779101438064b9a77cabc5fc485d2
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-09-26 15:10:48 +0200

    Remove null:false constraint for user name (temporary)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926124931) do
+ActiveRecord::Schema.define(version: 20170926130910) do
 
   create_table "todos", force: :cascade do |t|
     t.text "item", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20170926124931) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.string "psid", null: false
   end
 
