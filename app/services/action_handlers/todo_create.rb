e72c667b390c7a369666a28c3d7e188ad4a8720c
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-09-27 12:36:04 +0200

    Create notification if time given

--- app/services/action_handlers/todo_create.rb
+++ app/services/action_handlers/todo_create.rb
@@ -5,6 +5,7 @@ module ActionHandlers
       current_user = User.where(psid: @parsed_response[:psid]).first_or_create!
       todo = Todo.create(item: @parsed_response.dig(:parameters, :item), user: current_user)
       if todo.persisted?
+        todo.notifications.create(notify_at: notify_time) if time_given?
         {
           text: 'Item successfully added to Your list.'
         }
@@ -13,6 +14,22 @@ module ActionHandlers
           text: 'Something went wrong, please try once again. Sorry!'
         }
       end
+    end
+
+    private
+
+    def notify_time
+       parsed_datetime = DateTime.parse time_param
+
+       @notify_time ||= parsed_datetime
+    end
+
+    def time_given?
+      time_param.present? && time_param != 'refused'
+    end
+
+    def time_param
+      @time_param ||= @parsed_response.dig(:parameters, :notify_at, :datetime)
     end
   end
 end
